9bf05ca2793362852d7e7e778f399df4ba948a78
Jackalob <nike9712@gmail.com>
2022-07-02 22:52:43 +0800

    progress 137

--- react-performance/src/exercise/02.js
+++ react-performance/src/exercise/02.js
@@ -3,7 +3,9 @@
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
+// import {getItems} from '../filter-cities'
+import {getItems} from '../workerized-filter-cities'
+import {useAsync} from '../utils'
 import {useForceRerender} from '../utils'
 
 function Menu({
@@ -60,8 +62,13 @@ function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
-  // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = getItems(inputValue)
+  // 🐨 wrap   in a call to `React.useMemo`
+  const {data: allItems, run} = useAsync({data: [], stats: 'pending'})
+
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
+  
   const items = allItems.slice(0, 100)
 
   const {
